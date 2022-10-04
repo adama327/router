f4a5eecf4cabf823a3e919a0112f9c1512a1ee0a
adama327 <adama327@gmail.com>
2022-10-04 17:33:19 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import React from "react";
+import {Suits,SuitsC,Narcos,NarcosC,Thebl,TheblC} from "./movielist";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App() {
+ const Home = ()=>{
+    return(
+      <>
+      <div>
+        <h2 style={{textAlign:"center"}}>Home of movies</h2>
+      </div>
+      <div className="mvlist">
+        <div className="suits">
+        <Suits/>
+        </div>
+   <div className="narcos">
+   <Narcos/>
+   </div>
+<div className="theb">
+<Thebl/>
+</div>
+
+      </div>
+     
+      </>
+    )
+  }
+return(
+  <>
+  <div>
+  </div>
+  <div>
+  <Routes>
+       <Route path='/' element={<Home />}></Route>
+       <Route index path='/suitsc' element={<SuitsC />}></Route>
+       <Route index path='/narcosc' element={<NarcosC />}></Route>
+       <Route index path='/theblacklistc' element={<TheblC />}></Route>
+     </Routes>
+  </div>
+  </>
+)
 }
 
-export default App;
+
+
+export default App
---
+++ src/movielist.js
@@ -0,0 +1,158 @@
+import React from "react";
+import {useNavigate } from "react-router-dom";
+
+export const Suits = () =>{
+    const Navigate = useNavigate()
+    return (
+        <>
+        <div>
+            <img onClick={()=> Navigate("/suitsc")} src="https://pictures.betaseries.com/fonds/poster/e5e10c0387c8b4e4e08ec4d372c5a59d.jpg" alt="" width={"350px"} height={"400px"} />
+            <div>
+                <h2 className="title">Suits</h2>
+                <h4 className="desct">Serie d'avocats sur mesure</h4>
+            </div>
+        </div>        
+        </>
+    )
+}
+
+export const SuitsC = () =>{
+    const Navigate = useNavigate()
+    return(
+        <>
+        <div className="cont">
+        <div>
+    <button onClick={()=> Navigate('/') }>Go home</button>
+       </div>
+<div>
+<div className="filmc">
+        <img src="https://images.news18.com/ibnlive/uploads/2021/07/1626853667_suits-poster.jpg?impolicy=website&width=510&height=356" alt="" width={"500px"} />
+        <div className="descdiv">
+        <h4>Mike Ross est un jeune homme brillant dont le rêve d'enfant était de devenir avocat, rêve brisé après que des imprévus l'eurent poussé à abandonner ses études. Intelligent et doté d'une mémoire eidétique, Mike subvient à ses besoins en passant des examens à la place d'autres personnes, en particulier des examens en droit.
+Harvey Specter est quant à lui un des meilleurs avocats de New York, qui vient d'obtenir une promotion au sein du cabinet d'avocats Pearson Hardman : il est alors tenu d'engager un assistant. Sur la base d'un quiproquo, Harvey fait passer un entretien d'embauche à Mike. Il est particulièrement impressionné par les qualités du jeune homme et l'engage. Comme Mike n'a pas de diplôme en droit alors que la société Pearson Hardman embauche exclusivement des diplômés de Harvard, tous deux décident dès lors de mentir et prétendre que Mike est un ancien de l'école, même si cela les expose à des poursuites judiciaires si la vérité est découverte.
+À son arrivée au cabinet, Mike est continuellement harcelé par Louis Litt, un rival de Harvey qui semble douter de ses références. Cependant, il peut compter sur l'amitié de Rachel Zane, une assistante juridique trop anxieuse pour passer l'examen du barreau, avec qui il développe une complicité</h4>
+       
+    </div>
+        </div>
+    <div>
+    <h2>Suits</h2>
+   
+    <span>rate : 89% </span>
+</div>
+</div>
+        </div>
+        
+
+        </>
+    )
+
+}
+
+
+export const Narcos = () =>{
+    const Navigate = useNavigate()
+    return (
+        <>
+        <div>
+            <img onClick={()=> Navigate("/narcosc")} src="https://fr.web.img6.acsta.net/pictures/15/07/29/14/33/086520.jpg" alt="" width={"350px"} height={"400px"}/>
+            <div>
+                <h2 className="title">Narcos</h2>
+                <h4 className="desct">Pablo Escobar, cette série raconte la guerre sanglante des cartels en Colombie.</h4>
+            </div>
+        </div>        
+        </>
+    )
+}
+
+export const NarcosC = () =>{
+    const Navigate = useNavigate()
+    return(
+        <>
+        <div className="cont">
+        <div>
+    <button onClick={()=> Navigate('/') }>Go home</button>
+       </div>
+<div>
+<div className="filmc">
+        <img src="https://i.pinimg.com/736x/28/fb/7a/28fb7a9999ebe7653082770d287d4943--pablo-escobar-streetwear.jpg" alt="" width={"500px"} />
+        <div className="descdiv">
+        <h4>À la fin des années 1970, les États-Unis et la Colombie se lancent dans une lutte acharnée contre le cartel de drogue de Medellín.
+             Steve Murphy, jeune agent de la DEA, fait son possible en compagnie de Javier Peña pour faire tomber Pablo Escobar et ses hommes,
+              malgré l'importante corruption policière qui gangrène la Colombie. 
+             Cette lutte se mêle à celle des États-Unis contre le communisme et à de nombreux autres intérêts politiques.
+             L'agent Peña a été promu chef de la DEA en Colombie après le "succès" de la traque d'Escobar. 
+             Dans le même temps, le cartel de Cali est devenu le leader sur le marché de la cocaïne.
+              Ayant infiltré les hautes sphères de la société colombienne, 
+              le cartel entend rediriger ses affaires vers des activités légales en capitalisant sur son immense fortune amassée grâce au trafic de drogue. 
+              Il débute alors des négociations à son avantage auprès du gouvernement colombien pour sa reddition.
+             </h4>
+       
+    </div>
+        </div>
+    <div>
+    <h2>Narcos</h2>
+   
+    <span>rate : 99% </span>
+</div>
+</div>
+        </div>
+        
+        </>
+    )
+
+}
+
+
+
+export const Thebl = () =>{
+    const Navigate = useNavigate()
+    return (
+        <>
+        <div>
+            <img onClick={()=> Navigate("/theblacklistc")} src="https://fr.web.img5.acsta.net/pictures/17/08/30/11/50/532007.jpg" alt="" width={"350px"} height={"400px"} />
+            <div>
+                <h2 className="title">The Blacklist</h2>
+                <h4 className="desct">L'ex-agent du gouvernement Raymond "Red" Reddington est l'un des fugitifs les plus recherchés au monde. Il accepte de collaborer avec le FBI à condition de travailler avec la profileuse Elizabeth Keen.</h4>
+            </div>
+        </div>        
+        </>
+    )
+}
+
+export const TheblC = () =>{
+    const Navigate = useNavigate()
+    return(
+        <>
+ 
+<div className="cont">
+<div>
+    <button onClick={()=> Navigate('/') }>Go home</button>
+       </div>
+<div>
+<div className="filmc">
+        <img src="http://img.over-blog-kiwi.com/0/66/79/72/20141005/ob_1f03b7_blacklist-the-blacklist-coming-soon-to.jpeg" alt="" width={"500px"} />
+        <div className="descdiv">
+        <h4>La série, qui se déroule principalement à Washington, raconte l'histoire d'un ancien officier de l'US Navy, Raymond « Red » Reddington,
+             qui a disparu vingt ans plus tôt pour devenir l'un des dix fugitifs les plus recherchés du FBI et un criminel de premier plan, surnommé le "Médiateur du crime". 
+            Reddington se rend volontairement au FBI, dans son quartier général.
+Cependant, il insiste pour travailler exclusivement avec une jeune profileuse récemment recrutée par le FBI, Elizabeth Keen. Les mystères de la vie de Reddington et de Liz,
+ ainsi que son intérêt pour elle, sont progressivement révélés au fil de l'évolution de la série. 
+ Chaque épisode présente l'un des criminels mondiaux et Reddington aide l'équipe à les suivre et à les appréhender.
+Keen s’interroge sur l’intérêt soudain que Reddington lui porte, bien qu’il soutienne que Keen est très spéciale. 
+Après que le FBI a fait tomber un terroriste sur lequel il a fourni des informations, Reddington révèle que ce terroriste n’est que le premier de beaucoup d’autres à venir : durant les deux dernières décennies, 
+il a dressé une liste des criminels et terroristes qu'il sait introuvables par le FBI parce que ce dernier ignorait purement et simplement leur existence. Or ce sont pour lui les criminels les plus importants. 
+Reddington surnomme cette liste « La Liste noire » (« The Blacklist »).</h4>
+       
+    </div>
+        </div>
+    <div>
+    <h2>The Blacklist</h2>
+   
+    <span>rate : 79% </span>
+</div>
+</div>
+</div>
+        </>
+    )
+
+}
